f59d0e2df1b7fe61e292aa1dc4e1958acb9aab25
Raphael Lima <raphaellima365@gmail.com>
2022-10-15 16:09:06 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Department obj = new Department(1, "Projetos");
+		
+		Seller seller = new Seller(29, "PH", "PHgmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
